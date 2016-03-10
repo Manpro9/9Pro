db83db2fd45c8fce5da3a46c723037150afed494
daniel anggraito <daniel.anggraito@ti.ukdw.ac.id>
2016-03-10 11:59:55 +0700

    Ini File dashboard untuk user

---
+++ dasbboard/js/agenda.js
@@ -0,0 +1,11 @@
+function addNow() {
+  nowDate = moment().tz("Europe/London").format('YYYY-MM-DD');
+  nowTime = moment().tz("Europe/London").format('HH:mm:ss');
+  document.getElementById('registration-date').value = nowDate;
+  document.getElementById('registration-time').value = nowTime;
+  set = setTimeout(function () { addNow(); }, 1000);
+}
+
+function stopNow() {
+  clearTimeout(set);
+}
