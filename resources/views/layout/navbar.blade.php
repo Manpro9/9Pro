d6fb398ec33c18a74f536a7b63a9aee4779e77bd
WahyuHadiii <wahyu.hadi@si.ukdw.ac.id>
2016-04-20 16:35:43 +0700

    nambah dikit, atur tampilan index

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -16,33 +16,11 @@
         <ul class="nav navbar-nav">
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="{{ asset('public/images/dist/user2-160x160.jpg') }}" class="user-image" alt="User Image">
+              <i class="fa fa-user" style="color:white;margin-top:30%;"></i>
               <span class="hidden-xs">{{ Auth::user()->name }}</span>
             </a>
             <ul class="dropdown-menu">
-              <li class="user-header">
-                <img src="{{ asset('public/images/dist/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
-                  <p>
-                    {{ Auth::user()->name }}
-                  </p>
-              </li>
-              <li class="user-body">
-                <div class="row">
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Followers</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Sales</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Friends</a>
-                  </div>
-                </div>
-              </li>
               <li class="user-footer">
-                <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
-                </div>
                 <div class="pull-right">
                   <a href="{{ url('/logout') }}" class="btn btn-default btn-flat">Sign out</a>
                 </div>
