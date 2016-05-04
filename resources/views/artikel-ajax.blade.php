6b97006a78f9a06d056b61501dc1df97a365e3ef
bimaa <bima.haryanto@ti.ukdw.ac.id>
2016-05-04 23:15:20 +0700

    1. tambah link ke detail di index

--- resources/views/artikel-ajax.blade.php
+++ resources/views/artikel-ajax.blade.php
@@ -1,10 +1,19 @@
 <div class="artikel-ajax">
 	<div class="row" style="padding-left:2%;">
 		@foreach($dataArtikel as $data)
-			<div class="col-sm-6" style="border:1px solid black; width:48%; margin-left:0px; padding-left:0px;padding-right:0px;">
-				<img src="{{ asset($data->image) }}" class="topic" alt="gambar1">
-				<h5 style="text-align:center;"><b>{{ $data->title }}</b></h5>
-			</div>
+			<?php 
+				$title = str_slug($data['title']);
+				if ($data['type'] == 'berita')
+					$route = 'berita.show';
+				else if ($data['type'] == 'kegiatan')
+					$route = 'kegiatan.show';
+			?>
+			<a href="{{ route($route, $title) }}">
+				<div class="col-sm-6" style="border:1px solid black; width:48%; margin-left:0px; padding-left:0px;padding-right:0px;">
+					<img src="{{ asset($data->image) }}" class="topic" alt="gambar1">
+					<h5 style="text-align:center;"><b>{{ $data->title }}</b></h5>
+				</div>
+			</a>
 		@endforeach
 	</div>
 </div>
