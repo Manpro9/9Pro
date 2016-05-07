23f245d8af3d7c10f36bd4376d961e659da20a61
Tude Prawira <tude.prawira@gmail.com>
2016-05-07 14:49:34 +0700

    Tambah halaman error404 Akses melalui /notfound

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -46,6 +46,9 @@ Route::group(['middleware' => ['web']], function () {
 	Route::get('/profil', function() {
 	    return view('content.profil');
 	});
+	Route::get('/notfound', function() {
+	    return view('content.error404');
+	});
 
 	//Route untuk tampilkan detail Berita, Pengumuman, Kegiatan, Gallery
 	Route::get('/berita/{id}', ['as' => 'berita.show', 'uses' => 'BeritaController@show' ]);
---
+++ resources/views/content/error404.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.master')
+
+<!-- CUSTOM CSS UNTUK GALLERY -->
+@section('css')
+    <link rel="stylesheet" href="{{ asset('public/css/404.css' )}}">
+@endsection
+
+
+<!-- CUSTOM CONTENT UNTUK GALLERY -->
+@section('content')
+<body marginwidth="0" marginheight="0">
+    <div id="block_error">
+        <div>
+         <h2>Error 404. &nbspMaaf halaman yang anda cari tidak ada.</h2>
+        <p>
+        Bla bla bla blb bla bla bla bla blablablablablabla bla abl blabla bla blabla bla 
+        </p>
+        <p>
+        Klik link berikut untuk kembali ke <a href="#">halaman utama</a>.
+        Atau pilih halaman lainnya melalui navigasi di sebelah kiri.
+        </p>
+        </div>
+    </div>
+</body>
+@endsection
