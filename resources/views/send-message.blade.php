eb020b6a6b3966d012028f839655590031f0a849
bimaa <bima.haryanto@ti.ukdw.ac.id>
2016-05-11 22:04:42 +0700

    CONTOH

--- resources/views/send-message.blade.php
+++ resources/views/send-message.blade.php
@@ -1,3 +1,8 @@
-<span>Dari: .... </span>
-<br>
-<span>Pesan: ... </span>
+<div class="notification-sender">
+	<div class="border" style="width: 500px; border-radius: 2%; border: 2px solid #017CC2; background-color: #A2D9F9; padding: 20px;">
+		<h1 style="text-align: center;"> PSDM UKDW </h1>
+		<hr>
+		<p style="font-size: 24px;"><strong>E-mail</strong><span class="sender-email" style="padding-left: 6px;">: {{ $email }}</span></p>
+		<p style="font-size: 24px;"><strong>Pesan</strong><span class="sender-message" style="padding-left: 12px;">: {{ $content }}</span></p>
+	</div>
+</div>
